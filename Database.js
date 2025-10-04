b6af65337958921528013615c6fca898597a545d
Ankit Mod <148241800+Ankitmod4@users.noreply.github.com>
2025-10-04 23:28:22 +0530

    Change database user and host for Sequelize
    Updated database connection credentials and host.

--- Database.js
+++ Database.js
@@ -1,9 +1,10 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('yesbro', 'root', 'root', {
-  host: 'mysql_db', 
+const sequelize = new Sequelize('yesbro', 'admin', 'ankit123', {
+  host: 'database-rds.ci16cm2gu6v2.us-east-1.rds.amazonaws.com', // your RDS endpoint
   dialect: 'mysql',
-  port: 3306, 
+  port: 3306,
+  logging: false, // optional: removes SQL logs from console
 });
 
 module.exports = sequelize;
