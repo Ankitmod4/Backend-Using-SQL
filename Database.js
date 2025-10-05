d986a06965b83935dc8e8431b66502d0593036ff
Ankit Mod <148241800+Ankitmod4@users.noreply.github.com>
2025-10-05 23:11:32 +0530

    Update Database.js

--- Database.js
+++ Database.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize('yesbro', 'admin', 'ankit123', {
-  host: 'database-rds.ci16cm2gu6v2.us-east-1.rds.amazonaws.com', // your RDS endpoint
+  host: 'database-1.ci16cm2gu6v2.us-east-1.rds.amazonaws.com', // your RDS endpoint
   dialect: 'mysql',
   port: 3306,
   logging: false, // optional: removes SQL logs from console
